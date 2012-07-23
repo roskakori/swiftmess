72fc437b1b2746170d2929e7d50d0a12eb9246a7
Thomas Aglassinger <roskakori@users.sourceforge.net>
2012-07-23 04:57:05 +0200

    Added parser for EDITFACT / ISO 15022 messages.

--- swiftmess.py
+++ swiftmess.py
@@ -20,3 +20,121 @@ import logging
 _log = logging.getLogger('swift')
 
 __version__ = '0.1'
+
+
+class Error(Exception):
+    pass
+
+
+def messageItems(readable):
+    '''
+    Message items found in ``readable`` as tuples of the form ``(nestingLevel, type, value)`` with:
+
+    * nestingLevel: level of nested blocks
+    * type: one of: 'message', 'block', 'field', 'value'
+    * value: for 'block' this is the block key, for 'field' this is the field name, 'value' this is the value
+      and for 'message' this is ``None``.
+
+    The messages have to be stored in EDIFACT / ISO 15022 format.
+    '''
+    assert readable is not None
+
+    # Possible values for ``state``.
+    _BeforeStartOfBlock = 'BeforeStartOfBlock'
+    _InBlockKey = 'InBlockKey'
+    _InLine = 'InLine'
+    _InFieldKey = 'InFieldKey'
+    _InFieldValue = 'InFieldValue'
+    _InValue = 'InValue'
+
+    state = _BeforeStartOfBlock
+    text = None
+    blockKey = None
+    fieldKey = None
+    level = 0
+
+    char = readable.read(1)
+    while (char != ''):
+        _log.debug('level=%d, char=%r, state=%s, text=%r', level, char, state, text)
+        if char == '\r':
+            pass
+        elif state == _BeforeStartOfBlock:
+            if char == '{':
+                state = _InBlockKey
+                level += 1
+                text = ''
+            elif char == '}':
+                if level == 0:
+                    raise Error('unmatched %r outside of any block must be removed' % char)
+                level -= 1
+            elif char == '\n':
+                if level != 0:
+                    raise Error(u'nested block must be closed (state=%r, level=%d)' % (state, level))
+                yield (level, 'message', None)
+            elif char != '\r':
+                raise Error('block must start with %r instead of %r' % ('{', char))
+        elif state == _InBlockKey:
+            if char == ':':
+                state = _InLine
+                blockKey = long(text)
+                yield (level, 'block', blockKey)
+                text = None
+            elif char.isdigit():
+                text += char
+            else:
+                raise Error('block id must consist of decimal digits bug encountered %r' % char)
+        elif state == _InLine:
+            if char == '{':
+                state = _InBlockKey
+                level += 1
+                text = ''
+            elif char == '}':
+                state = _BeforeStartOfBlock
+                level -= 1
+                assert level >= 0
+            elif char == ':':
+                state = _InFieldKey
+                text = ''
+            elif char == '\n':
+                yield (level, 'value', '')
+            else:
+                state = _InValue
+                text = char
+        elif state == _InFieldKey:
+            if char == ':':
+                state = _InFieldValue
+                fieldKey = text
+                text = ''
+            else:
+                text += char
+        elif state == _InFieldValue:
+            if (char == '\n') or char == '}':
+                yield (level, 'field', fieldKey)
+                yield (level, 'value', text)
+                fieldKey = None
+                text = None
+                if char == '}':
+                    state = _BeforeStartOfBlock
+                    level -= 1
+                    assert level >= 0
+                else:
+                    state = _InLine
+            else:
+                text += char
+        elif state == _InValue:
+            if (char == '\n') or char == '}':
+                yield (level, 'value', text)
+                text = None
+                if char == '}':
+                    state = _BeforeStartOfBlock
+                    level -= 1
+                    assert level >= 0
+                else:
+                    state = _InLine
+            else:
+                text += char
+        char = readable.read(1)
+    if state != _BeforeStartOfBlock:
+        raise Error(u'block must be closed (state=%r)' % state)
+    if level != 0:
+        raise Error(u'nested block must be closed (state=%r, level=%d)' % (state, level))
--- test/test_swiftmess.py
+++ test/test_swiftmess.py
@@ -14,15 +14,69 @@
 #
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+from __future__ import with_statement
+
+import logging
+import os
 import unittest
+import swiftmess
+
+_log = logging.getLogger('swift')
+
+
+def _testFilePath(name):
+    basePath = os.path.dirname(__file__)
+    return os.path.join(basePath, name)
 
 
 class TestSwiftmess(unittest.TestCase):
 
-    def testCanDoNothing(self):
-        pass
+    def testCanReadMessageItems(self):
+        ExpectedItems = [
+            (1, 'block', 1L),
+            (1, 'value', 'F01XXXXXXXXXXXX0000999999'),
+            (1, 'block', 2L),
+            (1, 'value', 'O5981519051128XXXXXXXXXXXX000099999905112815 19N'),
+            (1, 'block', 3L),
+            (2, 'block', 108L),
+            (1, 'block', 4L),
+            (1, 'value', ''),
+            (1, 'field', '20'),
+            (1, 'value', '99990212189999'),
+            (1, 'field', '12'),
+            (1, 'value', '001'),
+            (1, 'field', '77E'),
+            (1, 'value', '/TREF XXXXXXXXXXXXXXXX'),
+            (1, 'value', '/NOIM 000000'),
+            (1, 'value', '/NOII 000000'),
+            (1, 'value', '/NOVM 000000'),
+            (1, 'value', '/NOVI 000000'),
+            (1, 'value', '/TRNA RAWCE290'),
+            (1, 'value', '-'),
+            (0, 'message', None),
+            (1, 'block', 1L),
+            (1, 'value', 'F01XXXXXXXXXXXX0000999999'),
+            (1, 'block', 2L),
+            (1, 'value', 'O5981519051128XXXXXXXXXXXX000099999905112815 19N'),
+            (1, 'block', 3L),
+            (2, 'block', 108L),
+            (1, 'block', 4L),
+            (1, 'value', ''),
+            (1, 'field', '20'),
+            (1, 'value', '99990212189999'),
+            (1, 'field', '12'),
+            (1, 'value', '099'),
+            (1, 'field', '77E'),
+            (1, 'value', '/NOMS 000001'),
+            (1, 'value', '-'),
+            (0, 'message', None)
+        ]
+        with open(_testFilePath('rawce290.txt')) as testFile:
+            actualItems = list(swiftmess.messageItems(testFile))
+            self.assertEqual(actualItems, ExpectedItems)
 
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
     #import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
